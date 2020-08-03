1760e5542784bdced2df2cd7442610c53ccfc039
vanzellam <52088121+vanzellam@users.noreply.github.com>
2020-08-02 22:10:22 -0300

    Add files via upload

---
+++ Aula 06/js/exercise.js
@@ -0,0 +1,30 @@
+var numPositivo = 5;
+var numNegativo = -2;
+var numDecimal = 1.4;
+var name = 'nome';
+var verdadeiro = true;
+
+console.log(numPositivo);
+console.log(numNegativo);
+console.log(numDecimal);
+console.log(name);
+console.log(verdadeiro);
+
+var nome = 'John'
+var sobrenome = 'Doe'
+console.log(nome, sobrenome);
+
+
+var texto = 'primeira frase \nsegunda frase';
+console.log(texto);
+
+var valorNulo = null;
+var naodefinido;
+console.log(valorNulo, naodefinido);
+
+var arr = ['uva', 'maçã', 'banana', 'laranja', 'manga'];
+for(i in arr){
+    console.log(arr[i]);
+}
+
+var dhTexto = "Digital House, a escola de programação que forma novas gerações de coders e profissionais digitais para imaginar, inovar e criar o que sempre sonharam."
---
+++ Aula 06/js/script.js
@@ -0,0 +1,62 @@
+/* comenta tudo dentro
+var nome = 'Joe';
+var idade = 21;
+var verdade = true;
+
+var nomeCompleto = 'Joe Smith';
+
+var lista = [1, 2, 3];
+var objeto = {
+    nome: 'Joe',
+    sobrenome: 'Smith'
+}
+
+var x; //comentarios
+
+var num1 = 10;
+var num2 = 5;
+
+var result = num1 + num2;
+
+console.log(result);
+
+
+
+var idade = 65;
+
+if((idade >= 18) && (idade<= 70)){
+    console.log("Voto obrigatório");
+}
+else{
+    console.log("Voto não obrigatório");
+}
+
+
+var mes = 7;
+
+switch(mes){
+    case 1: console.log('Janeiro');
+            break;
+    case 2: console.log('Fevereiro');
+            break;
+    case 3: console.log('Março');
+            break;
+    case 4: console.log('Abril');
+            break;
+    default: console.log('Mês inválido!');
+}
+
+
+var diaDaSemana = 'Segunda-feira';
+
+diaDaSemana == 'Domingo'
+? console.log('Hoje é dia de churrasco!')
+: console.log('Hoje não é dia de churrasco!')
+*/
+
+var numero = 0;
+
+while(numero < 3){
+    console.log(numero);
+    numero++;
+}
